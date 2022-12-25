9d6a13558c7d0a426a86163d2139f737dcbc0a8b
awaCon <awaconteh95@gmail.com>
2022-12-25 10:08:43 -0500

    Added a class called reverse String

---
+++ src/chapter1/ReverseString.java
@@ -0,0 +1,30 @@
+package chapter1;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        String str = "raceCar, Awa, madam, Sarjo ";
+        System.out.print("Original word: ");
+        System.out.println("raceCar");
+        String res = "";
+        char ch ;// char variable to store the characters
+        for (int i = str.length()-1; i>=0; i-- ){
+            ch = str.charAt(i);//extracts each character
+            res+=ch; //adds each character in front of the existing string
+        }
+        System.out.println("Reverse words are: " + res);
+    }
+
+   /*
+     public static String reverseString(String str) {
+        String result = "";
+        for (int i = str.length()-1; i >= 0; i--) {
+            result += str.charAt(i);
+
+        }
+            return result;
+
+    }
+    */
+}
